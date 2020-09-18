a921c72f26ef4bef77eae48d8238bbbdeb1d6803
Hackers20 <70922207+Hackers20@users.noreply.github.com>
2020-09-19 02:51:49 +0700

    Hack Fb

---
+++ Hackfb.sh
@@ -0,0 +1,59 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+figlet HACK FB
+
+echo $cyan"========================================="
+echo $green"    [ Author : Hacker20 ]"
+echo $green"    [ Github : github.com/hacker20 ]"
+echo $cyan"========================================="
+sleep 2
+echo $cyan"Login Facebook[+]"
+echo
+echo
+sleep 1
+echo -n $blue"Ussername : ";read s
+sleep 1
+echo -n $green"Password : ";read i
+echo
+#savephphsil.php
+sleep 3
+echo $red"Sedang Mengecek Akun"
+echo
+sleep 4
+echo $cyan"Akun Berhasil Di Cek"
+echo
+sleep 1
+echo $green"Sedang Login"
+echo
+sleep 5
+echo -n $purple"ID Target : ";read g
+echo
+sleep 4
+echo $blue"Sedang Membobol Password Target"
+echo
+sleep 1
+echo $yellow"Password Target Di Temukan"
+sleep 1
+echo
+echo
+echo "============================="
+echo "ID =" $g
+echo "Password = Alanweaker29"
+echo "============================="
+sleep 4
+echo $cyan"Sedang Keluar Dari Tools"
+echo
+sleep 7
+clear
+figlet B.Y.E
+echo $blue"============================================="
+echo $green"Jangan Salahgunakan Tools Ini"
+echo $green"Jangan Di Record Hargai Usaha Seseorang"
+echo $blue"============================================="
